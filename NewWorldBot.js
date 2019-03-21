630f1209635745e85f7601e41513dcca062ec999
Shawn <ShawnBaumbach@gmail.com>
2019-03-21 07:30:17 -0500

    New Features, parties and welcome messages
    !party # and a automatic welcome message

--- NewWorldBot.js
+++ NewWorldBot.js
@@ -7,6 +7,18 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`); 
 });
 
+// Just Dead Things
+client.on("guildMemberAdd", member => {
+    if (member.guild.id == '246768916381171723')
+        member.guild.channels.get('397896637361553408').send(member.user.username + " has joined the server! He's been on discord since " 
+        + (member.user.createdAt.getMonth() + 1)
+        + "/" + (member.user.createdAt.getDate() + 1)
+        + "/" + member.user.createdAt.getFullYear()); 
+
+
+    //console.log(member.client.users)
+});
+
 client.on('message', message  => {
     //var guild = client.guilds.get(guildID);
     //const channel = member.guild.channels.find(ch => ch.name === 'work-orders');
@@ -18,8 +30,11 @@ client.on('message', message  => {
     //    message.delete(60000);
         //console.log(message.type.toString())
 
-    if (message.content === "bad bot")
-        message.reply(`Just doing my job. :angry: `);
+    if (message.content.toLowerCase === "bad bot")
+        message.reply(`Just doing my job! :angry: `);
+
+    if (message.content.toLowerCase === "good bot")
+        message.reply(`You bet. :smirk: `);
 
     if (message.content.substring(0, 1) == '!') {
         var args = message.content.substring(1).split(' ');
@@ -99,9 +114,69 @@ client.on('message', message  => {
                 message.channel.send(message.guild.roles.filter(g => g.name != "@everyone" 
                 && bBots.Roles.includes(g.name.toUpperCase()) == true).map(g => g.name).join("\n"));
                 break;
+
+            case 'PARTY':
+                console.log(message.member.guild.members.map(m=>m.displayName));
+                if (cmdparameter != null){
+                    // returns false if the parameter does not contain a number
+                    if (isNaN(cmdparameter) == true) {
+                        message.channel.send("Please provide a number following the Party command");
+                        break;
+                    }
+                    
+                    let max = Math.floor(cmdparameter);
+                    /*
+                    if (max <= 1) {
+                        message.channel.send("Please provide a number larger than 1");
+                        break;
+                    }
+                    */
+                    let names = []
+                    let partymembers = message.member.guild.members.map(m=>m.voiceChannelID).map(m=>m == message.member.voiceChannelID);
+                    let namestemp = message.member.guild.members.map(m=>m.displayName);
+                    for (let pm = 0; pm < partymembers.length; pm++) {
+                        if (partymembers[pm] == true)
+                            names.push(namestemp[pm]);
+                    }
+                    
+                    console.log(teamString(names, max))
+                    message.channel.send(teamString(shuffle(names), max))
+
+                } else {
+                    message.channel.send("There must be a number following the 'Party' command")
+                }
+                break;
             }
         }
     
+    function teamString(names, max) {
+        let str = [];
+        let temp = 0
+        let teamNumber = 1;
+
+        str.push("Team " + teamNumber + ":");
+        names.forEach(e => {
+            //if (temp < max)
+            temp < max ? str.push("\t" + e) : ( str.push("Team " + (++teamNumber) + ":"), str.push("\t" + e), temp = 0 )
+            temp ++;
+        });
+
+        return str;
+    }
+
+    function shuffle(a) {
+        b = [];
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+
+        return a;
+    }
+
     function getRoleID(name){
         //var guild = client.guilds.get(guildID);
         var guild = message.guild;
